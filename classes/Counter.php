de6e5b37d4920afbcc80b992e3d68d61ffe14452
Krzysztof Pazdur <pazdurk@gmail.com>
2015-02-18 01:26:14 +0100

    1 of 2 done
    After few hours with xdebug I done 1 task of 2.  I think..  :hand:

--- classes/Counter.php
+++ classes/Counter.php
@@ -1,14 +1,28 @@
 <?php
 
-namespace efikCounter;
+namespace PCAcademyHR;
 use PDO;
 use PDOException;
 
-class Counter {
+/**
+ * Class Counter
+ * @package efikCounter
+ */
+class Counter
+{
 
+    /**
+     * @var PDO
+     */
     public $DBH;
+    /**
+     * @var
+     */
     private static $instance;
 
+    /**
+     * @param $ConfigArray
+     */
     public  function  __construct(&$ConfigArray)
     {
 
@@ -36,12 +50,71 @@ class Counter {
 
     }
 
+    /**
+     * @param $CONFIG
+     * @return Counter
+     */
     public static function getInstance(&$CONFIG)
     {
         if (!isset(self::$instance)){
             self::$instance = new Counter($CONFIG);
         }
         return self::$instance;
+    }
+
+
+    /**
+     * @param $sql
+     * @return array
+     */
+    public function fetchDbData($sql)
+    {
+        $temp_res = array();
+        $stm = $this->DBH->query($sql);
+
+        if($stm == FALSE) { die("FALSE"); exit; }
+        while ($row = $stm->fetch()) {
+            $temp_res[$row['DATE']] = $row['READ'];
+        }
+        return $temp_res;
+    }
+
+
+    /**
+     * @param $data
+     * @return bool
+     */
+    public function chceckOneBeforeData($data)
+    {
+        $sql = "SELECT `DATE`, `READ` FROM ELECTRICITY_METER_READS WHERE `DATE` <= '$data'";
+
+        $fdata[] = $this->fetchDbData($sql);
+        $keys = count($fdata);
+        return ($keys > 0 ? $fdata[$keys-1] : FALSE );
+    }
+
+
+    /**
+     * @param $start
+     * @param $end
+     * @return array
+     */
+    public function getDataBetween($start, $end)
+    {
+        $sql    = "SELECT `DATE`, `READ`  FROM ELECTRICITY_METER_READS WHERE ( `DATE` BETWEEN '$start' AND '$end')";
+        $sql2   = "SELECT `DATE`, `READ`  FROM ELECTRICITY_METER_READS WHERE ( `DATE` == '$start'";
+
+        if($this->DBH->exec($sql2) == FALSE) {
+            $tab_a = $this->chceckOneBeforeData($start);
+            $tab_b = $this->fetchDbData($sql);
+            return array_merge($tab_a,$tab_b);
+        }
+        else{
+            return $this->fetchDbData($sql);
+        }
+
+
+
     }
 
 
--- index.php
+++ index.php
@@ -7,12 +7,13 @@
 
     require_once('config.php');
     require('./classes/Counter.php');
+    require('./vendor/autoload.php');
 
-
-   use efikCounter\Counter;
+   use PCAcademyHR\Counter;
 
     $counter = Counter::getInstance($config);
     unset($config);
 
-
+    $a = $counter->getDataBetween("2014-01-15", "2014-02-15");
+       print_r($a);
 
